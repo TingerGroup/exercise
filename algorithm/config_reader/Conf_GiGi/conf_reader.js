002400b654791cf3bf63b8dd80ce122e52041e76
gigi <snailzhao1995@163.com>
2017-04-15 11:37:14 +0800

    update 2017/4/15

--- algorithm/config_reader/Conf_GiGi/conf_reader.js
+++ algorithm/config_reader/Conf_GiGi/conf_reader.js
@@ -16,7 +16,7 @@ module.exports = ConfigReader;
 
 function ConfigReader(filePath){
 	this.filePath = filePath;
-	this.content = new Content();
+	this.content = new Content(filePath);
 }
 
 ConfigReader.prototype.init = function(onsuccess) {
@@ -30,74 +30,39 @@ ConfigReader.prototype.init = function(onsuccess) {
 	});	
 };
 
-
-
 ConfigReader.prototype.getItemValue = function(key) {
 	return this.content.getItemValue(key);
 };
 
-/**
- * An method to set value by specified item
- *
- * @param {String} key
- * @param {String} value
- */
-
 ConfigReader.prototype.setItemValue = function(key , value) {
-	// body...
+	return this.content.setItemValue(key , value);
 };
 
-/**
- * An method to add new item
- *
- * @param {String} key
- * @param {String} value
- */
-
-ConfigReader.prototype.addNewItem = function(key , value) {
-	// body...
+ConfigReader.prototype.addNewItem = function(new_key , new_value) {
+	return this.content.addNewItem(new_key , new_value);
 };
 
-/**
- * An method to add item note
- *
- * @param {String} key
- * @param {Note} note
- */
-
-ConfigReader.prototype.addItemNote = function(key , note) {
-	// body...
+ConfigReader.prototype.addItemNote = function(key , new_note_content) {
+	return this.content.addItemNote(key , new_note_content);
 };
-
-/**
- * An method to remove specified item
- *
- * @param {String} key
- */
 
 ConfigReader.prototype.removeItem = function(key) {
-	// body...
-};
-
-/**
- * An method to save the updatest configuration file
- *
- */
-
-ConfigReader.prototype.toSave = function() {
-	// body...
+	return this.content.removeItem(key);
 };
 
 /**
  * An object to describe a configuration file
  */
 
-function Content(){
+function Content(filePath){
+	this.filePath = filePath;
 	this.content = new Array();
 }
 
 /**
  * An method to parse file content by lines
+ *
+ * @param {String} data
  */
 
 Content.prototype.getContent = function(data) {
@@ -113,54 +78,155 @@ Content.prototype.getContent = function(data) {
 
 Content.prototype.getItemValue = function(key) {
 
-	for(var i = 0; i < this.content.length; i++){
-		if(0 == this.content[i].indexOf(key)){
+	var itemPos = this.isExist(key);
 
-			var valuePos = this.content[i].indexOf('=');
-			return this.content[i].slice(valuePos+1);
-		}
+	if(itemPos < 0){
+		throw new Error('The ' + key + ' is not existed.');
 	}
-	throw new Error('The ' + key + ' is not exited.');
+	var valuePos = this.content[itemPos].indexOf('=');
+	return this.content[itemPos].slice(valuePos+1);
+	
 };
+
+/**
+ * An method to set value by specified item
+ *
+ * @param {String} key
+ * @param {String} value
+ */
 
 Content.prototype.setItemValue = function(key , value) {
-	// body...
+
+	var itemPos = this.isExist(key);
+
+	if(itemPos < 0){
+		throw new Error('The ' + key + ' is not existed.');
+	}
+
+	var item = new Item(key , value);
+	this.content[itemPos] = item.toSave();
+	var string = this.content.join('\n');
+	this.toSave(string);
+	
 };
 
-Content.prototype.addNewItem = function(key , value) {
-	// body...
+/**
+ * An method to add new item
+ *
+ * @param {String} new_key
+ * @param {String} new_value
+ */
+
+Content.prototype.addNewItem = function(new_key , new_value) {
+
+	if(this.isExist(new_key) > 0){
+		throw new Error('The ' + new_key + ' is existed.')
+	}	
+
+	var item = new Item(new_key , new_value);
+	this.content.push(item.toSave());
+	var string = this.content.join('\n');
+	this.toSave(string);
+
 };
 
-Content.prototype.addItemNote = function(key , value) {
-	// body...
+/**
+ * An method to add item note
+ *
+ * @param {String} key
+ * @param {String} new_note_content
+ */
+
+Content.prototype.addItemNote = function(key , new_note_content) {
+	
+	var itemPos = this.isExist(key);
+	var note = new Note(new_note_content); 
+
+	if(itemPos < 0){
+		throw new Error('The ' + key + ' is not existed.');
+	}
+
+	this.content.splice(itemPos,0,note.toSave());
+	var string = this.content.join('\n');
+    this.toSave(string);
+	
 };
 
+/**
+ * An method to remove specified item
+ *
+ * @param {String} key
+ */
+
+Content.prototype.removeItem = function(key) {
+	
+	var itemPos = this.isExist(key);
+
+	if(itemPos){
+		var note_item = new Note(this.content[itemPos]);
+		this.content[itemPos] = note_item.toSave();
+		var string = this.content.join('\n');
+	    this.toSave(string);
+	}
+};
+
+/**
+ * An method to save all changes to file
+ *
+ * @param {String} string
+ */
+
+Content.prototype.toSave = function(string) {
+	
+	fs.writeFile(this.filePath, string, 'utf8', (err)=>{
+		if(err){
+			throw err;
+		}
+	})
+};
+
+/**
+ * An method to check the key is exist in configuration file or not
+ *
+ * @param {String} key
+ */
+
+Content.prototype.isExist = function(key) {
+
+	for(var i = 0; i<this.content.length; i++){
+
+		if(0 == this.content[i].indexOf(key)){
+			return i;
+		}
+	}
+	return -1;
+};
+
+/*
+ * An object to describe note
+ */
 
 function Note(content){
-	this.content = contemt;
+	this.content = content;
 
-	this.toSave = toSave;
-	function toSave(){
+	this.toSave = function(){
 		return ('#' + this.content);
 	}
 }
+
+/*
+ *An method to describe configuration item
+ */
 
 function Item(key , value){
 	this.key = key;
 	this.value = value;
 
-	this.toSave = toSave;
-	function toSave(){
+	this.toSave = function(){
 		return (this.key + '=' + this.value);
 	}
 }
 
-function BlankLine(){
-	this.toSave = toSave;
-	function toSave(){
-		return '\n';
-	}
-}
 
 
 
--- algorithm/config_reader/Conf_GiGi/test.js
+++ algorithm/config_reader/Conf_GiGi/test.js
@@ -7,7 +7,11 @@ var conf_reader = require('./conf_reader.js');
 var test = new conf_reader(filePath);
 
 test.init(function(){
-	console.log(test.getItemValue('1DSHELL'));
+	console.log(test.getItemValue('DSHELL'));
+	test.setItemValue('TESTKEY','testSuccess');
+	test.addNewItem('NEWKEY','testSuccess');
+	test.addItemNote('NEWKEY','This is a test item');
+	test.removeItem('NEWKEY');
 });
 
 
